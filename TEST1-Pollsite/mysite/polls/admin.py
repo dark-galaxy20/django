a8e406e38d1e28ec7021920bf43b110cb6131f4a
dark-galaxy2 <minekhorteam@gmail.com>
2020-06-28 15:28:46 +0430

    class,teacher

--- TEST1-Pollsite/mysite/polls/admin.py
+++ TEST1-Pollsite/mysite/polls/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from .models import Question,Choice,Student,Classes,Teacher
 
-# Register your models here.
-from .models import Question,Choice,Student
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_name','student_last_name','student_age','student_class')
+    search_fields = ['student_last_name','student_name']
+    list_filter = ('student_name', 'student_age','student_class')
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text','votes',)
 
 admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Student)
+admin.site.register(Choice,ChoiceAdmin)
+admin.site.register(Classes)
+admin.site.register(Teacher)
+admin.site.register(Student,StudentAdmin)
--- TEST1-Pollsite/mysite/polls/models.py
+++ TEST1-Pollsite/mysite/polls/models.py
@@ -20,13 +20,24 @@ class Choice(models.Model):
         return self.choice_text
 
 #students-|new|
+class Teacher(models.Model):
+    teacher_full_name = models.CharField(max_length=12,null=True)
+    teacher_age = models.IntegerField(null=True)
+    def __str__(self):
+        return self.teacher_full_name
+class Classes(models.Model):
+    class_number = models.CharField(max_length=4,null=True)
+    class_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE,null=True)
+    def __str__(self):
+        return self.class_number
 class Student(models.Model):
     student_name = models.CharField(max_length=12)
-    student_last_name = models.CharField(max_length=12,default="No data")
-    student_idnum = models.CharField(max_length=15,default="No data")
+    student_last_name = models.CharField(max_length=12,null=True)
+    student_age = models.IntegerField(null=True)
+    student_class = models.ForeignKey(Classes, on_delete=models.CASCADE,null=True)
+    student_idnum = models.CharField(max_length=15,null=True)
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.student_name
-    
 # Create your models here.
 # version 1
