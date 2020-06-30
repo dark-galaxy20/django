b75c17cd4b963ead7a888e51a2c4d9737ae27ca9
dark-galaxy2 <minekhorteam@gmail.com>
2020-06-30 21:45:19 +0430

    .

--- TEST1-Pollsite/mysite/polls/admin.py
+++ TEST1-Pollsite/mysite/polls/admin.py
@@ -1,17 +1,45 @@
 from django.contrib import admin
-from .models import Question,Choice,Student,Classes,Teacher
-
-
+from .models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+def is_teacher(user):
+    return user.groups.filter(name='teacher').exists()
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('student_name','student_last_name','student_age','student_class')
+    list_display = ('student_name','student_last_name','student_class')
     search_fields = ['student_last_name','student_name']
     list_filter = ('student_name', 'student_age','student_class')
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('choice_text','votes',)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('exam_title','exam_branch',)
+    list_filter = ('exam_branch', )
+class ScoreAdmin(admin.ModelAdmin):
+    if User.is_superuser or is_teacher(request.user) :
+        list_filter = ('user', 'exam',)
+    list_display = ('exam','user','score',)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser or is_teacher(request.user) :
+            return qs
+        return qs.filter(user=request.user)
 
-admin.site.register(Question)
-admin.site.register(Choice,ChoiceAdmin)
-admin.site.register(Classes)
+class StudentInline(admin.StackedInline):
+    model = Student
+    can_delete = False
+    verbose_name_plural = 'student'
+class TeacherInline(admin.StackedInline):
+    model = Teacher
+    can_delete = False
+    verbose_name_plural = 'teacher'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (StudentInline,TeacherInline)
+
+
+
+admin.site.register(Clas)
+admin.site.register(Branch)
 admin.site.register(Teacher)
 admin.site.register(Student,StudentAdmin)
+admin.site.register(Exam)
+admin.site.register(Score,ScoreAdmin)
--- TEST1-Pollsite/mysite/polls/models.py
+++ TEST1-Pollsite/mysite/polls/models.py
@@ -1,43 +1,46 @@
 from django.db import models
 import datetime
-
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
 
 #students-|new|
-class Teacher(models.Model):
-    teacher_full_name = models.CharField(max_length=12,null=True)
-    teacher_age = models.IntegerField(null=True)
-    def __str__(self):
-        return self.teacher_full_name
-class Classes(models.Model):
+class Clas(models.Model):
     class_number = models.CharField(max_length=4,null=True)
-    class_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE,null=True)
     def __str__(self):
         return self.class_number
+class Branch(models.Model):
+    branch_name = models.CharField(max_length=12,null=True)
+    def __str__(self):
+        return self.branch_name
+class Teacher(models.Model):
+    teacher_full_name = models.CharField(max_length=12,null=True)
+    teacher_age = models.PositiveSmallIntegerField(null=True)
+    teacher_branch = models.ForeignKey(Branch,on_delete=models.SET_NULL,null=True)
+    teacher_class = models.ForeignKey(Clas,on_delete=models.SET_NULL,null=True)
+    user = models.OneToOneField(User,on_delete=models.SET_NULL,null=True)
+    def __str__(self):
+        return self.teacher_full_name
+
 class Student(models.Model):
     student_name = models.CharField(max_length=12)
     student_last_name = models.CharField(max_length=12,null=True)
-    student_age = models.IntegerField(null=True)
-    student_class = models.ForeignKey(Classes, on_delete=models.CASCADE,null=True)
-    student_idnum = models.CharField(max_length=15,null=True)
-    pub_date = models.DateTimeField("date published")
+    student_age = models.PositiveSmallIntegerField(null=True)
+    student_class = models.ForeignKey(Clas,on_delete=models.SET_NULL,null=True)
+    user = models.OneToOneField(User,on_delete=models.SET_NULL,null=True)
     def __str__(self):
         return self.student_name
+class Exam(models.Model):
+    exam_title = models.CharField(max_length=20,null=True)
+    exam_branch = models.ForeignKey(Branch,on_delete=models.SET_NULL,null=True)
+    exam_date = models.DateTimeField('exam date')
+    def __str__(self):
+        return self.exam_title
+class Score(models.Model):
+    exam = models.ForeignKey(Exam,on_delete=models.CASCADE,null=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    score = models.FloatField(null=True)
+    def __str__(self):
+        return str(self.score)
 # Create your models here.
 # version 1
